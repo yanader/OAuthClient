b98e3023d0457e06be26c732077eba34d8e6ca96
ste <mrbarrow@hotmail.co.uk>
2024-09-15 13:26:49 +0100

    changed success URL

--- src/main/java/com/oauth/sample/SecurityConfig.java
+++ src/main/java/com/oauth/sample/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
                 //.formLogin(form -> form.defaultSuccessUrl("/hello", true));
-                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/hello", true));
+                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:3000/dashboard", true));
         return http.build();
     }
 
